67ec328cf1a7a52c415c1b9a9dee196e65587859
AnkurSingh007 <AnkurSingh007@users.noreply.github.com>
2014-12-21 21:40:02 +0530

    Create radixsort.cpp
    This is sorting for positive integers only. It is linear time sorting routine. Only integers of size 32 bits are considered

---
+++ src/radixsort.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+
+int getIntAtPos(int num, int pos){
+	if(pos == 0)return num%10;
+	else{
+		return ((num - (num % (int)pow(10, pos))) / 10) % 10;
+	}
+}
+
+
+void countingsort(int *arr, int size, int pos){
+	int temp[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	int k = 0;
+	for(int j = 0; j < 10; j++){
+		for(int i = 0; i < size; i++){
+				if(getIntAtPos(arr[i], pos) == j){
+					int temp = arr[i];
+					arr[i] = arr[k];
+					arr[k++] = temp;
+				}
+		}
+	}
+	
+	}
+	
+}
+
+
+
+int main(){
+	cout<<"How many Numbers do you want to enter"<<endl;
+	int num = 0;
+	int max;//not initialised
+	int maxNumofDigits = 0;
+	cin>>num;
+	int *arr = new int[num];
+	cout<<"Enter the list of numbers"<<endl;
+	for(int i = 0; i <num; i++){
+		cin>>arr[i];
+		if(i == 0)max = arr[i];
+		else if(max < arr[i]){
+			max = arr[i];
+		}
+	}
+	while(max != 0){
+		max /= 10;
+		maxNumofDigits += 1;
+	}
+	for(int i = 0; i < maxNumofDigits; ++i){
+		countingsort(arr, num, i);
+	}
+	
+	cout<<"----------------------------------------------"<<endl;
+	cout<<"sorted array is"<<endl;
+	for(int i = 0; i < num; i++){
+		cout<<arr[i]<<endl;
+	}
+	return 0;
+}
+
